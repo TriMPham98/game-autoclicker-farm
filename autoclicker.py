bb67fbb632f2958455731ada76c35928a1c73060
Tri <trimpham98@gmail.com>
2023-05-18 21:49:25 -0700

    Change click location

--- autoclicker.py
+++ autoclicker.py
@@ -8,7 +8,7 @@ clickLoop = True
 while clickLoop:
     for i in range(100):
         pyautogui.PAUSE = 0.01
-        pyautogui.moveTo(240, 970)
-        pyautogui.leftClick()   
+        pyautogui.moveTo(1280, 746)
+        pyautogui.leftClick()
     pyautogui.moveTo(490, 930)
     pyautogui.leftClick()

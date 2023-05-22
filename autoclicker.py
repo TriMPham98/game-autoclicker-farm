61c81b1697349747259742110ef5f14399952a3a
Tri <trimpham98@gmail.com>
2023-05-22 09:18:05 -0700

    Add autoupgrader and print click number

--- autoclicker.py
+++ autoclicker.py
@@ -1,14 +1,22 @@
 import pyautogui
 import time
 
-pyautogui.FAILSAFE
+pyautogui.FAILSAFE = True
 
 clickLoop = True
+clickCount = 0
 
 while clickLoop:
-    for i in range(100):
+    for i in range(500):
         pyautogui.PAUSE = 0.01
         pyautogui.moveTo(1280, 746)
         pyautogui.leftClick()
-    pyautogui.moveTo(490, 930)
+        clickCount += 1
+        print(f"Click: {clickCount}")
+        
+    pyautogui.moveTo(185, 960)
     pyautogui.leftClick()
+    pyautogui.moveTo(1280, 1065)
+    pyautogui.leftClick()
+    
+    clickCount = 0  # Reset click count at the end of each loop
